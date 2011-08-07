be304f7308220bfbf0adf1abf85540847163dcfd
sue445 <sue445@sue445.net>
2011-08-07 16:18:44 +0900

    8章まで

--- src/tdddev/Money.java
+++ src/tdddev/Money.java
@@ -1,13 +1,25 @@
 package tdddev;
 
-public class Money {
+public abstract class Money {
 
 	protected int amount;
 
 	@Override
 	public boolean equals(Object obj) {
 		Money money = (Money)obj;
-		return amount == money.amount;
+		return amount == money.amount
+				&& getClass().equals(money.getClass());
 	}
+
+	public static Money dollar(int amount) {
+		return new Dollar(amount);
+	}
+
+	public static Money franc(int amount) {
+		return new Franc(amount);
+	}
+
+	public abstract Money times(int multiplier) ;
+
 
 }
