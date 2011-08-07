23d6791950de6c471783e4e10824a5728725992e
sue445 <sue445@sue445.net>
2011-08-07 15:58:54 +0900

    3章まで

--- src/tdddev/Dollar.java
+++ src/tdddev/Dollar.java
@@ -13,4 +13,10 @@ public class Dollar {
 		return new Dollar(amount * multiplier);
 	}
 
+	@Override
+	public boolean equals(Object obj) {
+		Dollar dollar = (Dollar)obj;
+		return amount == dollar.amount;
+	}
+
 }
